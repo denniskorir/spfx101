081040bad7217223dd1603150e9796aab879275b
denniskorir <dkdenniskorir@gmail.com>
2018-01-12 13:21:13 +0300

    SPFX project boilerplate scaffolded

---
+++ src/webparts/welcome/WelcomeWebPart.ts
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'WelcomeWebPartStrings';
+import Welcome from './components/Welcome';
+import { IWelcomeProps } from './components/IWelcomeProps';
+
+export interface IWelcomeWebPartProps {
+  description: string;
+}
+
+export default class WelcomeWebPart extends BaseClientSideWebPart<IWelcomeWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IWelcomeProps > = React.createElement(
+      Welcome,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
